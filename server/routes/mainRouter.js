e7b78e6f47b4655e5c4af2c5d75e7481d89dc0da
am-aru <arpita.arusharma@gmail.com>
2024-06-19 13:25:39 +0530

    added log in main router

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { usersRouter } = require("./usersRoute");
 
+console.log("Main router")
+
 router.use("/user", usersRouter);
 
 module.exports = router;
