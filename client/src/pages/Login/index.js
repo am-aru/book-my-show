7289596e95bbf30f5bb143803bca679fba6cb8ce
Arpita Sharma <arpita.arusharma@gmail.com>
2024-09-16 21:44:20 +0530

    create email portion

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -4,7 +4,21 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>this is Login page.</div>
+    <>
+    <main className='App-header'>
+        <h1>Login to Book my Show</h1>
+        <section className='mw-500 text-center px-3'>
+       <form layout="vertical">
+        <Form.Item label="Email" htmlFor='email' name='email' className='d-block'
+        rules={[{ required:true, message:"email is required" }]} >
+         <Input type='email' id='email' name='email' placeholder='Enter your email'/>
+        
+        </Form.Item>
+
+       </form>
+        </section>
+    </main>
+    </>
   )
 }
 
